49e2d3b22a2b9691660a8605365b66eaec9c5fee
blericalopes <blericalopes@outlook.com>
2022-07-25 13:57:51 -0300

    Atualizado

--- src/cipher.js
+++ src/cipher.js
@@ -3,16 +3,17 @@ const cipher = {Criptar,Descriptar};
 function Criptar () {
   let texto = document.getElementById("texto").value.toUpperCase();
   let deslocamento = parseInt(document.getElementById("deslo").value);
-  
 console.log (texto); 
 console.log (deslocamento);
+
   let transfTextoCi = "";
     for( let i =0; i< texto.length; i++) { 
     let textoCifrado = ((texto.charCodeAt(i) - 65 + deslocamento) % 26) + 65;
-    transfTextoCi += String.fromCharCode(textoCifrado)
+    transfTextoCi += String.fromCharCode(textoCifrado);
+
     let elementoResultado = document.getElementById("resultado");
     let resultado = "A mensagem Criptografada é: " + 
-    transfTextoCi; elementoResultado.innerHTML = resultado
+    transfTextoCi; elementoResultado.innerHTML = resultado;
  }
  console.log (transfTextoCi);
 }
@@ -27,9 +28,10 @@ console.log (deslocamento);
     for( let i =0; i< texto.length; i++) { 
       let textoDecifrado = ((texto.charCodeAt(i) - 65 - deslocamento) % 26) + 65;
       transfTextoDe += String.fromCharCode(textoDecifrado); 
+
       let elementoResultado = document.getElementById("resultado");
-    let resultado = "A mensagem Descriptografada é: " + 
-    transfTextoDe; elementoResultado.innerHTML = resultado
+      let resultado = "A mensagem Descriptografada é: " + 
+      transfTextoDe; elementoResultado.innerHTML = resultado;
  }
  console.log (transfTextoDe);
 }
