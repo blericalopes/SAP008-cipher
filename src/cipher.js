f6c2ef6b9ab9382457e4a4e12b15dcf1609b0f2c
blericalopes <blericalopes@outlook.com>
2022-07-26 12:53:15 -0300

    Atualizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,15 @@
 const cipher = {encode, decode}; 
 function encode (offset,texto) { 
-  if (typeof offset !== "number" || typeof texto !== "string"){
-    throw new TypeError("Parametros invalidos")
+  if (typeof offset !== "number" || typeof texto !== "string"){//condição para receber tipo correto e disparar erro
+    throw new TypeError("Parametros invalidos")//com throw ele encerra e não precisa de else
   }
-  let transfTextoCi = "";
-  for (let i =0; i< texto.length; i++) { 
-    let textoCifrado = ((texto.charCodeAt(i) - 65 + offset) % 26) + 65; 
-    transfTextoCi += String.fromCharCode(textoCifrado); 
+  let transfTextoCi = "";//texto q vai ser transformado
+  for (let i =0; i< texto.length; i++) { //acessa letra por letra q foi digitada
+    let textoCifrado = ((texto.charCodeAt(i) - 65 + offset) % 26) + 65; //criada p identificar texto entre 65 e 90
+    transfTextoCi += String.fromCharCode(textoCifrado); //transf em string
    
   }
-return transfTextoCi;
+return transfTextoCi;//retorna 
 }
 function decode (offset,texto) { 
   if (typeof offset !== "number" || typeof texto !== "string"){
