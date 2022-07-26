3413909b746777fa46979527bed698d5f6799c35
blericalopes <blericalopes@outlook.com>
2022-07-26 11:18:28 -0300

    Atualizado testes ok

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,27 @@
-const cipher = {Criptar,Descriptar};
-
-function Criptar () {
-  let texto = document.getElementById("texto").value.toUpperCase();
-  let deslocamento = parseInt(document.getElementById("deslo").value);
-console.log (texto); 
-console.log (deslocamento);
-
+const cipher = {encode, decode}; 
+function encode (offset,texto) { 
+  if (typeof offset !== "number" || typeof texto !== "string"){
+    throw new TypeError("Parametros invalidos")
+  }
   let transfTextoCi = "";
-    for( let i =0; i< texto.length; i++) { 
-    let textoCifrado = ((texto.charCodeAt(i) - 65 + deslocamento) % 26) + 65;
-    transfTextoCi += String.fromCharCode(textoCifrado);
-
-    let elementoResultado = document.getElementById("resultado");
-    let resultado = "A mensagem Criptografada é: " + 
-    transfTextoCi; elementoResultado.innerHTML = resultado;
- }
- console.log (transfTextoCi);
+  for (let i =0; i< texto.length; i++) { 
+    let textoCifrado = ((texto.charCodeAt(i) - 65 + offset) % 26) + 65; 
+    transfTextoCi += String.fromCharCode(textoCifrado); 
+   
+  }
+return transfTextoCi;
 }
-
-function Descriptar () {
-  let texto = document.getElementById("texto").value.toUpperCase();
-  let deslocamento = parseInt(document.getElementById("deslo").value);
-console.log (texto); 
-console.log (deslocamento);
-
-  let transfTextoDe = "";
-    for( let i =0; i< texto.length; i++) { 
-      let textoDecifrado = ((texto.charCodeAt(i) - 65 - deslocamento) % 26) + 65;
-      transfTextoDe += String.fromCharCode(textoDecifrado); 
-
-      let elementoResultado = document.getElementById("resultado");
-      let resultado = "A mensagem Descriptografada é: " + 
-      transfTextoDe; elementoResultado.innerHTML = resultado;
- }
- console.log (transfTextoDe);
+function decode (offset,texto) { 
+  if (typeof offset !== "number" || typeof texto !== "string"){
+    throw new TypeError("Parametros invalidos")
+  }
+  let transfTextoDe = ""; 
+  for( let i =0; i< texto.length; i++) { 
+    let textoDecifrado = ((texto.charCodeAt(i) + 65 - offset) % 26) + 65; 
+    transfTextoDe += String.fromCharCode(textoDecifrado); 
+    
+  }
+  return transfTextoDe;
 }
-
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import cipher from './cipher.js';
-
-let descriptarBtn = document.getElementById("descriptar-btn");
-let criptarBtn = document.getElementById("criptar-btn");
-
-criptarBtn.addEventListener("click", () => {
-let criptarBtn = cipher.Criptar();
+import cipher from './cipher.js'; 
+const criptarBtn = document.getElementById("criptar-btn"); 
+criptarBtn.addEventListener("click", () => { 
+    const texto = document.getElementById("texto").value.toUpperCase(); 
+    const offset = parseInt(document.getElementById("deslo").value); 
+    const resultado = cipher.encode(offset,texto); 
+    const elResultado = document.getElementById("resultado"); 
+    const msg = "A mensagem Criptografada é: " + resultado; 
+    elResultado.innerHTML = msg; 
+   
 })
-
-descriptarBtn.addEventListener("click", () => {
-let descriptarBtn = cipher.Descriptar();   
+const descriptarBtn = document.getElementById("descriptar-btn"); 
+descriptarBtn.addEventListener("click", () => { 
+    const texto = document.getElementById("texto").value.toUpperCase(); 
+    const offset = parseInt(document.getElementById("deslo").value); 
+    const resultado = cipher.decode(offset,texto);
+    const elResultado = document.getElementById("resultado"); 
+    const msg = "A mensagem Descriptografada é: " + resultado; 
+    elResultado.innerHTML = msg; 
 })
 
